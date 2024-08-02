a5fcb785da8ecef9096ed301d1236d2a4f12a62e
German <gercger5@gmail.com>
2024-08-02 13:46:17 -0300

    ejercicio comunicacion entre componentes

--- src/app/gifs/components/card-list/card-list.component.ts
+++ src/app/gifs/components/card-list/card-list.component.ts
@@ -5,6 +5,7 @@ import { Gif } from '../../interfaces/gifs.interfaces';
 @Component({
   selector: 'gifs-card-list',
   templateUrl: './card-list.component.html',
+
 })
 
 export class CardListComponent {
